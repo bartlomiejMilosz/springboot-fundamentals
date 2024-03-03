b6e0d986fec2e85ae85ba03bcd37d2368486b383
Bart Milo <bartlomiej.milosz@gmail.com>
2024-03-03 10:16:32 +0100

    Add simple playbook maven project to get space for testing some scenarios

---
+++ playbook/src/main/java/io/github/bartmilo/playbook/Person.java
@@ -0,0 +1,11 @@
+package main.java.io.github.bartmilo.playbook;
+
+public class Person {
+    private String name;
+    private int age;
+    
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
